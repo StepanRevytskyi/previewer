41d96c42fa897f969f4b914fdc984494ef1b57dd
StepanRevytskyi <revytskyi.stepan@uzhnu.edu.ua>
2018-12-27 11:32:52 +0200

    refactor

--- app/src/main/java/com/arondillqs5328/moappspreviewer/model/ApplicationListPesponse.java
+++ app/src/main/java/com/arondillqs5328/moappspreviewer/model/ApplicationListPesponse.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class ApplicationListPesponse {
     @SerializedName("data")
     @Expose
-    private List<Datum> data = null;
+    private List<Datum> data;
 
     public ApplicationListPesponse() {
 
